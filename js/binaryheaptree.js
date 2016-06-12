a16047614f047f5f218310c9fb0b01d280b5d690
Brett DeWoody <brettdewoody@gmail.com>
2016-06-12 11:20:57 -0600

    Fixes & additions to heap methods.

--- js/binaryheaptree.js
+++ js/binaryheaptree.js
@@ -5,7 +5,7 @@ var BinaryHeapTree = function (tree) {
 BinaryHeapTree.prototype = {
   // Returns the index of the left child given a parent index
   lchild: function (index) {
-    if (this.tree.length > 2 * index) {
+    if (this.tree.length > (2 * index) + 1) {
       return ((2 * index) + 1);
     } else {
       return null;
@@ -15,7 +15,7 @@ BinaryHeapTree.prototype = {
 
   // Returns the index of the right child given a parent index
   rchild: function (index) {
-    if (this.tree.length > (2 * index) + 1) {
+    if (this.tree.length > (2 * index) + 2) {
       return ((2 * index) + 2);
     } else {
       return null;
@@ -31,13 +31,16 @@ BinaryHeapTree.prototype = {
     }
   },
 
-  //
+  // Pushes a value to the end of the heap and restores heap order
   push: function (value) {
-    this.tree.push(value);
-    this.float(this.tree.length - 1);
+    if (value >= 0) {
+      this.tree.push(value);
+      this.float(this.tree.length - 1);
+    }
   },
 
-  //
+  // Pops off the root node then moves the last node to the root
+  //  and restores heap order
   pop: function () {
     var value = this.tree[0];
     var last = this.tree.pop();
@@ -56,7 +59,7 @@ BinaryHeapTree.prototype = {
 
     var parentIndex = this.parent(index);
 
-    if (parentIndex != -1 && this.tree[parentIndex] > value) {
+    if (parentIndex != -1 && this.tree[parentIndex] >= value) {
       var parentValue = this.tree[parentIndex];
       this.tree[parentIndex] = value;
       this.tree[index] = parentValue;
@@ -75,13 +78,13 @@ BinaryHeapTree.prototype = {
       var lchildValue = this.tree[this.lchild(index)];
       if (lchildValue < value) {
         this.swap(this.lchild(index), index);
-        this.sink(this.lchild);
+        this.sink(this.lchild(index));
       }
     } else if (this.rchild(index) < size) {
       var rchildValue = this.tree[this.rchild(index)];
       if (rchildValue < value) {
         this.swap(this.rchild(index), index);
-        this.sink(this.rchild);
+        this.sink(this.rchild(index));
       }
     } else {
       this.tree.push(value);
@@ -89,9 +92,27 @@ BinaryHeapTree.prototype = {
 
   },
 
+  // Swaps two nodes given their indexes
   swap: function (index1, index2) {
     var value1 = this.tree[index1];
     this.tree[index1] = this.tree[index2];
     this.tree[index2] = value1;
+  },
+
+  // Returns the children of a node
+  children: function (index) {
+    var children = [];
+
+    if (this.lchild(index)) {
+      children.push(this.lchild(index));
+    } else {
+      return null;
+    }
+
+    if (this.rchild(index)) {
+      children.push(this.rchild(index));
+    }
+
+    return children;
   }
 }
