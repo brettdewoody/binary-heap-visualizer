5e38df45cb58505becd34839993fd62893711b48
Brett DeWoody <brettdewoody@gmail.com>
2016-06-12 23:26:57 -0600

    Style cleanup.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 var binaryheaptree = new BinaryHeapTree();
 var binaryheapvisualizer = new BinaryHeapVisualizer("svg");
 
+var handlePush = function() {
+  binaryheaptree.push(Number(document.getElementById('controls-input').value));
+  visualize();
+};
+
+var handlePop = function() {
+  var popped = binaryheaptree.pop();
+  alert('The popped value is ' + popped);
+  visualize();
+}
+
+var handleClear = function() {
+  binaryheaptree.clear();
+  visualize();
+}
+
 var visualize = function () {
   d3.select("svg").selectAll("*").remove();
   binaryheapvisualizer.drawTree(binaryheaptree, 0);
-}
+};
