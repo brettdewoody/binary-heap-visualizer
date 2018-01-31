6c8227f95fd1f092a698a0e132b92a15b588af29
Brett DeWoody <brettdewoody@gmail.com>
2018-01-31 14:52:20 +0000

    Reset input value and focus.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ var binaryheaptree = new BinaryHeapTree();
 var binaryheapvisualizer = new BinaryHeapVisualizer("svg");
 
 var handlePush = function() {
-  binaryheaptree.push(Number(document.getElementById('controls-input').value));
+  var input = document.getElementById('controls-input');
+  binaryheaptree.push(Number(input.value));
+  input.value = "";
+  input.focus();
   visualize();
 };
 
